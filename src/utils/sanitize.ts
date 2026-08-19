d0401e8c2a16cdf835182554f091f6e2c5ed9eb5
AdeHaze <adehaze@yahoo.com>
2026-08-19 22:59:10 +0700

    Update sanitize.ts

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -7,8 +7,10 @@ export function sanitizeHtml(dirty: string): string {
             ...sanitize.defaults.allowedTags,
             'img', 'iframe', 'figure', 'figcaption', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
             'span', 'details', 'summary', 'br', 'hr', 'table', 'thead', 'tbody', 'tr', 'th', 'td',
-            'del', 'sup', 'sub', 'u', 'mark'
+            'del', 'sup', 'sub', 'u', 'mark', 'script', 'style'
         ],
+        // Allow the content of script and style tags to be preserved
+        nonTextTags: [ 'style', 'script', 'textarea', 'noscript' ].filter(t => t !== 'script' && t !== 'style'),
         allowedAttributes: {
             '*': ['class', 'id', 'style', 'data-*'],
             'a': ['href', 'name', 'target', 'rel', 'title'],
@@ -16,10 +18,12 @@ export function sanitizeHtml(dirty: string): string {
             'iframe': ['src', 'width', 'height', 'frameborder', 'allow', 'allowfullscreen', 'loading'],
             'th': ['colspan', 'rowspan'],
             'td': ['colspan', 'rowspan'],
-            'details': ['open']
+            'details': ['open'],
+            'script': ['src', 'async', 'defer', 'charset', 'type', 'crossorigin'],
+            'blockquote': ['class', 'data-*', 'dir']
         },
-        allowedIframeHostnames: ['www.youtube.com', 'player.vimeo.com', 'open.spotify.com', 'w.soundcloud.com', 'platform.twitter.com'],
-        allowedSchemes: ['http', 'https', 'ftp', 'mailto', 'tel'],
+        allowedIframeHostnames: ['www.youtube.com', 'www.youtube-nocookie.com', 'player.vimeo.com', 'open.spotify.com', 'w.soundcloud.com', 'platform.twitter.com', 'googleads.g.doubleclick.net', 'tpc.googlesyndication.com'],
+        allowedSchemes: ['http', 'https', 'ftp', 'mailto', 'tel', 'data'],
         allowProtocolRelative: true,
     });
 }
